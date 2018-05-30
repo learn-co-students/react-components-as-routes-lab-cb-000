f46b2620eaafee49d4600e7d05c95a113c48bfea
theboymo <billfero@gmail.com>
2018-05-30 17:34:17 +0100

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,17 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      { directors.map((director, i) => {
+        return (
+          <div key={i} className="director">
+            <p>{ director.name}</p>
+            <ul>
+              { director.movies.map((movie, i)=> <li key={i}>{movie}</li>)}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
@@ -13,7 +10,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar />
+        <Router path="/" component={Home} exact />
+        <Router path="/actors" component={Actors} />
+        <Router path="/directors" component={Directors} />
+        <Router path="/movies" component={Movies} />
+      </div>
     </Router>
   );
 };
