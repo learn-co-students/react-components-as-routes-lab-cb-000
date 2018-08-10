d1f32e7cc1756a002a502948ed94ac519c3d3901
Heather Christman <efl7a@yahoo.de>
2018-08-10 14:28:59 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { directors } from '../data';
 
+const displayDirectors = () => directors.map(director => <div><h3>Director's Name: {director.name}</h3><ul>Movies: {displayMovies(director.movies)}</ul></div>)
+const displayMovies = (movies) => movies.map(movie => <li>{movie}</li>)
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {displayDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { movies } from '../data';
 
+const displayGenres = (genres) => genres.map(genre => <li>{genre}</li>)
+const displayMovies = () => movies.map(movie => <div><h2>Movie Title: {movie.title}</h2><h2>Movie Time: {movie.time}</h2><ul>Genres: {displayGenres(movie.genres)}</ul></div>)
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {displayMovies()}
     </div>
   );
 };
